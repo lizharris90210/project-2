583bf76953b036a426f775625fc64051d30d4d02
jpearce2014 <jlpearce@icloud.com>
2019-11-23 12:35:57 -0500

    Handlebars partials now functioning.  Modal max-height is no longer in conflict with materialize.  Working on onclick listener for modals now.

---
+++ public/js/design/navbar.js
@@ -0,0 +1,17 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {navBar();};
+
+// Get the header
+var header = document.getElementById("navBar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function navBar() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,44 @@
 var db = require("../models");
 
+// Requiring our custom middleware for checking if a user is logged in
+// var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 module.exports = function (app) {
-  // Load index page
-  app.get("/", function (req, res) {
+  
+  // =========================================================
+  //  BEGIN HTML Routing Info from Authentication Boilerplate
+  // =========================================================
+
+  // Loads Homepage (index.handlebars)
+  app.get("/", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.render("profile");
+    }
     res.render("index");
   });
+  //
+  // Because we are using modals for the login and sign up, we do not need this.
+  //
+  // app.get("/login", function(req, res) {
+  //   // If the user already has an account send them to the members page
+  //   if (req.user) {
+  //     res.redirect("/members");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/login.html"));
+  // });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  // app.get("/members", isAuthenticated, function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/members.html"));
+  // });
+
+  // =======================================================
+  //  END HTML Routing Info from Authentication Boilerplate
+  // =======================================================
   app.get("/homepage", function (req, res) {
     res.render("homepage");
-  });
-  app.get("/", function (req, res) {
-    res.render("index");
-  });
-
-  // Load the profile page
-  app.get("/profile", function(req, res){
-    res.render("profile");
   });
 
   // Load example page and pass in an example by id
