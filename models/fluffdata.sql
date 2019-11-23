8b914a31dfe03d6c230055a1ef61494586f5826f
Liz Harris <lizharris90210@gmail.com>
2019-11-23 10:37:06 -0500

    sequelize fixed

--- models/fluffdata.sql
+++ models/fluffdata.sql
@@ -1,65 +1,69 @@
 use cue_db;
 
 INSERT INTO Artists
-    (artist_name,location_city,location_state,contact_email,contact_insta,contact_twitter,current_band,instruments,past_bands,genres) VALUES
+(artist_name,location_city,location_state,contact_email,contact_insta,contact_twitter,current_band,instruments,past_bands,genres) 
+VALUES
 ('Liz Harris','Washington','DC','LizZepp@zmail.com','@LizZep','@LizZtweet','the VarChars','guitar','forLoops','classic rock');
-INSERT INTO `Artists`
-    (`artist_name`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`current_band`,`instruments`,`past_bands`,`genres`) VALUES
+INSERT INTO Artists
+(artist_name,location_city,location_state,contact_email,contact_insta,contact_twitter,current_band,instruments,past_bands,genres) 
+VALUES
 ('Hilary Lamb','Los Angeles','CA','indiePOP@zmail.com','@indiePOP','@indiePOPtweet','the Swiftees','lead singer','Tay Tays','indie, pop');
-INSERT INTO `Artists`
-    (`artist_name`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`current_band`,`instruments`,`past_bands`,`genres`) VALUES
+INSERT INTO Artists
+(artist_name,location_city,location_state,contact_email,contact_insta,contact_twitter,current_band,instruments,past_bands,genres) 
+VALUES
 ('Jackson Pearce','Miami','FL','json@zmail.com','@jackson5','@jackson5','the Action Jacksons','keyboard, guitar','the Apple Eaters','metal, indie, alt');
-INSERT INTO `Artists`
-    (`artist_name`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`current_band`,`instruments`,`past_bands`,`genres`) VALUES
+INSERT INTO Artists
+(artist_name,location_city,location_state,contact_email,contact_insta,contact_twitter,current_band,instruments,past_bands,genres) 
+VALUES
 ('Armon Roberts','New York','NY','AR15@zmail.com','@AR15','@AR15tweet','the NoDemons','drums, backup vocals','the Postals','rock, indie, pop');
 
-INSERT INTO ``
-    (`id`,`backstage_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`equipment`,`experience`,`genre`) VALUES
-(1,'Boris Karlov','Denver','CO','BorisKGB@zmail.com','@bkgb999','@boristweets999','audio board','2 tours with Ozzy, 1 tour with Kelly Clarkson, 3 tours with Ariana Grande','any');
-INSERT INTO ``
-    (`id`,`backstage_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`equipment`,`experience`,`genre`) VALUES
-(2,'Josh Hashings','Portland','OR','jhash@zmail.com','@jhashtags','@jhashtagstweets','lights, stage setup, stage teardown, guitar tuning','4 tours with Foo Fighters, 1 tour with Mariah Carey','any');
-INSERT INTO ``
-    (`id`,`backstage_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`equipment`,`experience`,`genre`) VALUES
-(3,'Lisa Simpson','Springfield','OH','lisasimpson@zmail.com	','@BleedingGumsJr','@LisaSax','manager','managed Jazz Stars 2 years, managed Blue Hounds 3 years, currently managing Dark Blue and Sad','jazz, blues');
-INSERT INTO ``
-    (`id`,`backstage_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`equipment`,`experience`,`genre`) VALUES
-(4,'Marty','Delmarva','DE','martyLovesToParty@zmail.com','@martyParty','@martyPartyTwitter','manager, stage setup, stage teardown','1 tour Mr. Universe','any');
+INSERT INTO Backstage
+(backstage_name,location_city,location_state,contact_email,contact_insta,contact_twitter,equipment,experience,genre) 
+VALUES
+('Boris Karlov','Denver','CO','BorisKGB@zmail.com','@bkgb999','@boristweets999','audio board','2 tours with Ozzy, 1 tour with Kelly Clarkson, 3 tours with Ariana Grande','any');
+INSERT INTO Backstage
+(backstage_name,location_city,location_state,contact_email,contact_insta,contact_twitter,equipment,experience,genre) 
+VALUES
+('Josh Hashings','Portland','OR','jhash@zmail.com','@jhashtags','@jhashtagstweets','lights, stage setup, stage teardown, guitar tuning','4 tours with Foo Fighters, 1 tour with Mariah Carey','any');
+INSERT INTO Backstage
+(backstage_name,location_city,location_state,contact_email,contact_insta,contact_twitter,equipment,experience,genre) 
+VALUES
+('Lisa Simpson','Springfield','OH','lisasimpson@zmail.com','@BleedingGumsJr','@LisaSax','manager','managed Jazz Stars 2 years, managed Blue Hounds 3 years, currently managing Dark Blue and Sad','jazz, blues');
+INSERT INTO Backstage
+(backstage_name,location_city,location_state,contact_email,contact_insta,contact_twitter,equipment,experience,genre) 
+VALUES
+('Marty','Delmarva','DE','martyLovesToParty@zmail.com','@martyParty','@martyPartyTwitter','manager, stage setup, stage teardown','1 tour Mr. Universe','any');
 
-INSERT INTO ``
-    (`id`,`group_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`current_group`,`artist_names`,`genres`) VALUES
-(1,'Dethklok','Mt. Olympus','unknown','dethklok@zmail.com	','@dethklokband','@dethklokband','Dethklok','Nathan Explosion,\nSkwisgaar Skwigelf,\nToki Wartooth,\nWilliam Murderface,\nPickles','metal');
-INSERT INTO ``
-    (`id`,`group_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`current_group`,`artist_names`,`genres`) VALUES
-(2,'Josie and the Pussycats','Riverdale','CA','JPCband@zmail.com	','@josiePC','@josiePCtweet','Josie and the Pussycats','Josie McCoy,  Melody Valentine, Valerie Brown','pop rock');
-INSERT INTO ``
-    (`id`,`group_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`current_group`,`artist_names`,`genres`) VALUES
-(3,'the Monkees','Hollywood','CA','heyheywerethemonkees@zmail.com','@heyheywerethemonkees','@heyheywerethemonkees','the Monkees','Micky Dolenz, Michael Nesmith,  Peter Tork, Davy Jones','pop rock');
-INSERT INTO ``
-    (`id`,`group_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`current_group`,`artist_names`,`genres`) VALUES
-(4,'the Venture Brothers	','Cherry Hill','New Jersey','vbros@zgmail.com	','@adventureBros','@ventureToAdventure','the Venture Brothers','Brock Samson, Hank Venture, Dean Venture, Rusty Venture','pop rock, pop punk');
+INSERT INTO Bands
+(band_name,location_city,location_state,contact_email,contact_insta,contact_twitter,current_band,artist_names,genres) 
+VALUES
+('Dethklok','Mt. Olympus','unknown','dethklok@zmail.com	','@dethklokband','@dethklokband','Dethklok','Nathan Explosion,Skwisgaar Skwigelf,Toki Wartooth,William Murderface,Pickles','metal');
+INSERT INTO Bands
+(band_name,location_city,location_state,contact_email,contact_insta,contact_twitter,current_band,artist_names,genres) 
+VALUES
+('Josie and the Pussycats','Riverdale','CA','JPCband@zmail.com','@josiePC','@josiePCtweet','Josie and the Pussycats','Josie McCoy, Melody Valentine, Valerie Brown','pop rock');
+INSERT INTO Bands
+(band_name,location_city,location_state,contact_email,contact_insta,contact_twitter,current_band,artist_names,genres) 
+VALUES
+('the Monkees','Hollywood','CA','heyheywerethemonkees@zmail.com','@heyheywerethemonkees','@heyheywerethemonkees','the Monkees','Micky Dolenz, Michael Nesmith, Peter Tork, Davy Jones','pop rock');
+INSERT INTO Bands
+(band_name,location_city,location_state,contact_email,contact_insta,contact_twitter,current_band,artist_names,genres) 
+VALUES
+('the Venture Brothers','Cherry Hill','New Jersey','vbros@zgmail.com','@adventureBros','@ventureToAdventure','the Venture Brothers','Brock Samson, Hank Venture, Dean Venture, Rusty Venture','pop rock, pop punk');
 
-INSERT INTO ``
-    (`id`,`venue_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`genres`) VALUES
-(1,'Chandelier','Los Angeles','CA','chandelier@zmail.com','@chandelierclub','@chandelierclub','rock, rap, hip hop, pop, dance, metal, punk');
-INSERT INTO ``
-    (`id`,`venue_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`genres`) VALUES
-(2,'Monopoly Arena','Dallas','TX','monopoly@zmail.com','@monopolyarea','@monopolyarea','all');
-INSERT INTO ``
-    (`id`,`venue_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`genres`) VALUES
-(3,'Backbar','Hogwash','ND','hogwash@zmail.com','@thehogwashbar','@thehogwashbar','country, rock, blues');
-INSERT INTO ``
-    (`id`,`venue_name
-`,`location_city`,`location_state`,`contact_email`,`contact_insta`,`contact_twitter`,`genres`) VALUES
-(4,'Green Room','Washington	','DC','green@zmail.com','@greenroom','@greenroom','jam, rock, dance, metal, jass, blues, pop');
+INSERT INTO Venues
+(venue_name,location_city,location_state,contact_email,contact_insta,contact_twitter,genres) 
+VALUES
+('Chandelier','Los Angeles','CA','chandelier@zmail.com','@chandelierclub','@chandelierclub','rock, rap, hip hop, pop, dance, metal, punk');
+INSERT INTO Venues
+(venue_name,location_city,location_state,contact_email,contact_insta,contact_twitter,genres) 
+VALUES
+('Monopoly Arena','Dallas','TX','monopoly@zmail.com','@monopolyarea','@monopolyarea','all');
+INSERT INTO Venues
+(venue_name,location_city,location_state,contact_email,contact_insta,contact_twitter,genres) 
+VALUES
+('Backbar','Hogwash','ND','hogwash@zmail.com','@thehogwashbar','@thehogwashbar','country, rock, blues');
+INSERT INTO Venues
+(venue_name,location_city,location_state,contact_email,contact_insta,contact_twitter,genres) 
+VALUES
+('Green Room','Washington','DC','green@zmail.com','@greenroom','@greenroom','jam, rock, dance, metal, jass, blues, pop');
