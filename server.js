544b5015d82ee824480599de67a9ef0b8a85b6f7
jpearce2014 <jlpearce@icloud.com>
2019-11-25 20:46:59 -0500

    Sign up works.  Fixed config.js problem.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-var session = require("express-session");
+var session = require("express-session"); // Required for passport (username/pw authentication)
 var db = require("./models");
-var passport = require("./config/passport");
+var passport = require("./config/passport"); // Required for passport (username/pw authentication)
 
 var PORT = process.env.PORT || 3000;
 var syncOptions = { force: false };
@@ -13,7 +13,7 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(session({secret: "keyboard cat", resave: true, saveUninitialized: true}));
+app.use(session({secret: "keyboard cat", resave: true, saveUninitialized: true})); //Required to keep track of our user's login status
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -27,6 +27,9 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+  // Separation of routing commands.  These requirements are necessary to ensure that any other changes made to the apiRoutes and htmlRoutes files does not mess up the modal connectivity to the database.
+  require("./routes/modal-apiRoutes")(app);
+  require("./routes/modal-htmlRoutes")(app);
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
@@ -39,8 +42,7 @@ db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT
-    );
+      PORT, PORT);
+    });
   });
-});
 module.exports = app;
